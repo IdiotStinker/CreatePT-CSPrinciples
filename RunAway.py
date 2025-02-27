a6c74fa9940445fa9446faec073a719e8a6eb6b0
IdiotStinker <saltylightning564@gmail.com>
2025-02-27 19:08:56 +0000

    puh

--- RunAway.py
+++ RunAway.py
@@ -1,17 +1,7 @@
 import random
-animatronicMap = [
-    [" ", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", " ",  "_", "_"],
-    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|", ".", "."],
-    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|", ".", "."],
-    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|", ".", "."],
-    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|", ".", "."],
-    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
-    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|", ".", "."],
-    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|", ".", "."],
-    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|", ".", "."],
-    ["\\", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "|", "_", "_"]
-]
+import os
+import math
+
 
 def spawnTable():
     tableSpawn = (random.randint(1, 9), random.randint(1, mapLength - 2))
@@ -28,7 +18,13 @@ def spawnTable():
         if random.randint(0, 1) == 0:
             animatronicMap[tableSpawn[0] + dy][tableSpawn[1] + dx] = "⑁"
     
+def spawnStunGun():
+    stunGunSpawn = (random.randint(1, 9), random.randint(1, mapLength - 2))
+    if animatronicMap[stunGunSpawn[0]][stunGunSpawn[1]] != ".":
+        spawnStunGun()
+        return
 
+    animatronicMap[stunGunSpawn[0]][stunGunSpawn[1]] = "г"
 
 def createMap():
     global knockedOut
@@ -37,6 +33,8 @@ def createMap():
     animatronicMap = [[], [], [], [], [], [], [], [], [], [], []]
     global mapLength
     mapLength = random.randint(20, 60)
+    global stunned
+    stunned = False
     #print(mapLength)
     doorSpot = random.randint(1, 9)
     for i in range(11):
@@ -74,18 +72,23 @@ def createMap():
     playerSpawn = (random.randint(1, 9), random.randint(1, 10))
     global playerPos
     playerPos = [playerSpawn[0], playerSpawn[1]]
-    #animatronicMap[playerSpawn[0]][playerSpawn[1]] = "P"
-    #animatronicMap[playerSpawn[0]][playerSpawn[1] + 1] = ""
+    animatronicMap[playerSpawn[0]][playerSpawn[1]] = "P"
+    
 
     fredSpawn = (random.randint(1, 9), random.randint(mapLength - 5, mapLength - 2))
     global fredPos
     fredPos = [fredSpawn[0], fredSpawn[1]]
-    #animatronicMap[fredSpawn[0]][fredSpawn[1]] = "F"
+    animatronicMap[fredPos[0]][fredPos[1]] = "F"
 
     for i in range(random.randint(3, 8)):
         spawnTable()
     
-    
+    for i in range(random.randint(2, 5)):
+        spawnStunGun()
+
+    animatronicMap[fredPos[0]][fredPos[1]] = "."
+    animatronicMap[playerSpawn[0]][playerSpawn[1]] = "."
+
 
 def printMap():
     for l, line in enumerate(animatronicMap):
@@ -103,28 +106,73 @@ def printMap():
         #print(printLine)
         print("".join(printLine))
 
+
+def grabStunGun(y, x):
+    animatronicMap[y][x] = "."
+    global stunCount
+    stunCount += 1
+    print(f"You collected one stun gun ammo! You currently have {stunCount}")
+
+def fireStun():
+    
+    global stunCount
+    if stunCount < 1:
+        print("You don't have any stuns!")
+        return
+    dist = ((fredPos[0]-playerPos[0])**2 + (fredPos[1]-playerPos[1])**2)**(1/2)
+    chance = 100 - math.log(dist) + dist
+    if dist > 30:
+        chance = 1
+    if random.randint(1, 100) < round(chance):
+        global stunned
+        stunned = True
+        print("Stunned!")
+    else:
+        if round(chance) < 0:
+            print(f"You missed with a 0% chance! Try firing closer!")
+        else:
+            print(f"You missed with a {round(chance)}% chance!")
+    
+    stunCount-=1
+
 def playerTurn():
+    grabbedStunGun = False
     global knockedOut
     if knockedOut:
         knockedOut = False
         return
     move = "defined"
-    while not move in ["w", "a", "s", "d"]:
-        move = input("What direction do you want to go? (wasd) ").lower()
+    while not move in ["w", "a", "s", "d", "fire"]:
+        if stunCount >= 1:
+            move = input("What direction do you want to go? (wasd), or would you like to use a stun gun (fire) ").lower()
+        else:
+            move = input("What direction do you want to go? (wasd) ").lower()
         if move == "w":
             if animatronicMap[playerPos[0]-1][playerPos[1]] != "." or [playerPos[0]-1, playerPos[1]] == fredPos:
+                if animatronicMap[playerPos[0]-1][playerPos[1]] == "г":
+                    grabbedStunGun = True
+                    break
                 move = "defined"
                 continue
         if move == "a":
             if animatronicMap[playerPos[0]][playerPos[1]-1] != "." or [playerPos[0], playerPos[1]-1] == fredPos:
+                if animatronicMap[playerPos[0]][playerPos[1]-1] == "г":
+                    grabbedStunGun = True
+                    break
                 move = "defined"
                 continue
         if move == "s":
             if animatronicMap[playerPos[0]+1][playerPos[1]] != "." or [playerPos[0]+1, playerPos[1]] == fredPos:
+                if animatronicMap[playerPos[0]+1][playerPos[1]] == "г":
+                    grabbedStunGun = True
+                    break
                 move = "defined"
                 continue
         if move == "d":
             if animatronicMap[playerPos[0]][playerPos[1]+1] != "." or [playerPos[0], playerPos[1]+1] == fredPos:
+                if animatronicMap[playerPos[0]][playerPos[1]+1] == "г":
+                    grabbedStunGun = True
+                    break
                 move = "defined"
                 continue
     
@@ -137,6 +185,15 @@ def playerTurn():
     if move == "d":
         playerPos[1] += 1
     
+    if move == "fire":
+        fireStun()
+        playerTurn()
+        return
+
+    terminalClear()
+
+    if grabbedStunGun:
+        grabStunGun(playerPos[0], playerPos[1])
     #printMap()
     #playerTurn()
 
@@ -151,91 +208,257 @@ def fredTurn():
     left = space[3]
     #UP RIGHT DOWN LEFT
 
-    while True:
-        if abs(dy) > abs(dx) and dy < 0:
+    dirs = 0
+    for bool in space:
+        if bool:
+            dirs += 1
+    
+    if dirs == 0:
+        print("woah! no movy")
+    
+    if dirs == 1:
+        for b, bool in enumerate(space):
+            if bool:
+                if b == 0:
+                    fredPos[0] -= 1
+                elif b == 1:
+                    fredPos[1] += 1
+                elif b == 2:
+                    fredPos[0] += 1
+                elif b == 3:
+                    fredPos[1] -= 1
+                
+                return
+    
+    
+
+    if abs(dy) > abs(dx) and dy < 0:
+        if up:
+            fredPos[0] -= 1
+            return
+        #dy = 0
+    if abs(dy) > abs(dx) and dy > 0:
+        if down:
+            fredPos[0] += 1
+            return
+        #dy = 0
+    if abs(dy) < abs(dx) and dx < 0:
+        if left:
+            fredPos[1] -= 1
+            return
+        #dx = 0
+    if abs(dy) < abs(dx) and dx > 0:
+        if right:
+            fredPos[1] += 1
+            return
+        #dx = 0
+    
+
+    if dy == 0:
+        if dx > 0 and not right:
+            if up and down:
+                if random.randint(0,1) == 0:
+                    fredPos[0]-=1
+                else:
+                    fredPos[0]+=1
+                return
+            
             if up:
                 fredPos[0] -= 1
-                return
-            dy = 0
-        if abs(dy) > abs(dx) and dy > 0:
+            
             if down:
-                fredPos[0] += 1
+                fredPos[0]+=1
+            
+            return
+        
+        if dx < 0 and not left:
+            if up and down:
+                if random.randint(0,1) == 0:
+                    fredPos[0]-=1
+                else:
+                    fredPos[0]+=1
                 return
-            dy = 0
-        if abs(dy) < abs(dx) and dx < 0:
+            
+            if up:
+                fredPos[0] -= 1
+            
+            if down:
+                fredPos[0]+=1
+            
+            return
+        
+    if dx == 0:
+        if dy > 0 and not down:
+            if left and right:
+                if random.randint(0,1) == 0:
+                    fredPos[1]-=1
+                else:
+                    fredPos[1]+=1
+                return
+            
             if left:
                 fredPos[1] -= 1
+            
+            if right:
+                fredPos[1]+=1
+            
+            return
+        if dy < 0 and not up:
+            if left and right:
+                if random.randint(0,1) == 0:
+                    fredPos[1]-=1
+                else:
+                    fredPos[1]+=1
                 return
-            dx = 0
-        if abs(dy) < abs(dx) and dx > 0:
+            
+            if left:
+                fredPos[1] -= 1
+            
+            if right:
+                fredPos[1]+=1
+            
+            return
+
+
+    #if abs(dy) == abs(dx):
+    #    print("woah")
+
+    if dx > 0 and dy > 0:
+        if (not right and down) or (right and not down):
             if right:
                 fredPos[1] += 1
                 return
-            dx = 0
-        
-        if abs(dy) == abs(dx):
-            if dx > 0 and dy > 0:
-                if (not right and down) or (right and not down):
-                    if right:
-                        fredPos[1] += 1
-                        break
-                    if down:
-                        fredPos[0] += 1
-                        break
+            if down:
+                fredPos[0] += 1
+                return
 
+        if not right and not down:
+            if abs(dy) == abs(dx):
+                #Go left or up
+                if random.randint(0, 1) == 0:
+                    fredPos[0] -= 1
+                else:
+                    fredPos[1] -= 1
+            #else go in specific
+            elif abs(dy) > abs(dx):
+                fredPos[1] -= 1
+            elif abs(dx) > abs(dy):
+                fredPos[0] -= 1
 
-                    
-            if dx > 0 and dy < 0:
-                if (not right and up) or (right and not up):
-                    if right:
-                        fredPos[1] += 1
-                        break
-                    if up:
-                        fredPos[0] -= 1
-                        break
-
-            if dx < 0 and dy > 0:
-                if (not left and down) or (left and not down):
-                    if left:
-                        fredPos[1] -= 1
-                        break
-                    if down:
-                        fredPos[0] += 1
-                        break
-
-            if dx < 0 and dy < 0:
-                if (not left and up) or (left and not up):
-                    if left:
-                        fredPos[1] -= 1
-                        break
-                    if down:
-                        fredPos[0] -= 1
-                        break
-
-
-        
-        if dy == 0 and dx == 0:
-            print("both spots clogged")
-            return
-            #if space
-            #if random.randint(0,1) == 0:
-
-        #else:
+        if right and down:
+            #Go right or down
+            if random.randint(0, 1) == 0:
+                fredPos[0] += 1
+            else:
+                fredPos[1] += 1
             
-    #elif abs(dx) > abs(dy):
+    if dx > 0 and dy < 0:
+        if (not right and up) or (right and not up):
+            if right:
+                fredPos[1] += 1
+                return
+            if up:
+                fredPos[0] -= 1
+                return
+        
+        if not right and not up:
+            if abs(dy) == abs(dx):
+                #Go left or down
+                if random.randint(0, 1) == 0:
+                    fredPos[0] += 1
+                else:
+                    fredPos[1] -= 1
 
+            elif abs(dy) > abs(dx):
+                fredPos[1] -= 1
+            elif abs(dx) > abs(dy):
+                fredPos[0] += 1
 
+        if right and up:
+            #Go right or up
+            if random.randint(0, 1) == 0:
+                fredPos[0] -= 1
+            else:
+                fredPos[1] += 1
+
+    if dx < 0 and dy > 0:
+        if (not left and down) or (left and not down):
+            if left:
+                fredPos[1] -= 1
+                return
+            if down:
+                fredPos[0] += 1
+                return
+        
+        if not left and not down:
+            if abs(dy) == abs(dx):
+                #Go right or up
+                if random.randint(0, 1) == 0:
+                    fredPos[0] -= 1
+                else:
+                    fredPos[1] += 1
+
+            elif abs(dy) > abs(dx):
+                fredPos[1] += 1
+            elif abs(dx) > abs(dy):
+                fredPos[0] -= 1
+
+        if left and down:
+            #Go left or down
+            if random.randint(0, 1) == 0:
+                fredPos[0] += 1
+            else:
+                fredPos[1] -= 1
+
+    if dx < 0 and dy < 0:
+        if (not left and up) or (left and not up):
+            if left:
+                fredPos[1] -= 1
+                return
+            if up:
+                fredPos[0] -= 1
+                return
+        
+        if not left and not up:
+            if abs(dy) == abs(dx):
+                #Go right or down
+                if random.randint(0, 1) == 0:
+                    fredPos[0] += 1
+                else:
+                    fredPos[1] += 1
+
+            elif abs(dy) > abs(dx):
+                fredPos[1] += 1
+            elif abs(dx) > abs(dy):
+                fredPos[0] += 1
+
+        if left and up:
+            #Go left or up
+            if random.randint(0, 1) == 0:
+                fredPos[0] -= 1
+            else:
+                fredPos[1] -= 1
+
+def terminalClear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def turn():
-    playerTurn()
-    fredTurn()
     printMap()
-
+    playerTurn()
+    global stunned
+    if not stunned:
+        fredTurn()
+    else:
+        if random.randint(0, 3) == 3:
+            stunned = False
     
 
+global stunCount
+stunCount = 0
+
+terminalClear()
 createMap()
-printMap()
-#playerTurn()
+
 while True:
     turn()
 
